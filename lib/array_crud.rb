f6add01757e887885fae751b2f7f60599a47d6ee
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-21 23:07:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ arrays  = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  iron man = create_an_array.shift
+  ("arrays") = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
